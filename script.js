2f81f528b45976f1aab196b60e4e8d5ba04609f4
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-01-16 10:36:56 -0600

    updated js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ var request = new XMLHttpRequest();
 request.open('GET', 'data.txt', false);
 request.send();
 console.log(request);
-document.getElementById('demo').innerHTML = request;
+document.getElementById('demo').innerHTML = request.responseText;
