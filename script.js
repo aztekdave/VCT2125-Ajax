83db3a4fc4d98949acd03ba2e16a89053ec746d8
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-01-16 11:17:29 -0600

    minor script changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-for (var i = 0; i<100; i++){
+for (var i = 0; i < 100; i++) {
 
-var request = new XMLHttpRequest();
-request.open('GET', 'data.txt', false);
-request.send();
+    var request = new XMLHttpRequest();
+    request.open('GET', 'data.txt', false);
 
-// document.getElementById('demo').innerHTML = request.responseText;
-if (request.status === 200 && request.readyState === 4) {
-    console.log(request);
-    document.writeln(request.responseText);
-}
+    request.onreadyStateChange = function () {
+
+
+        // document.getElementById('demo').innerHTML = request.responseText;
+        if (request.status === 200 && request.readyState === 4) {
+            console.log(request);
+            document.writeln(request.responseText);
+        }
+    }
+    request.send();
 }
