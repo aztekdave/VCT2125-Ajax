3ea9a536873dbe86589bfacfb58b574ebed702f7
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-01-16 11:19:49 -0600

    ok

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 for (var i = 0; i < 100; i++) {
-
     var request = new XMLHttpRequest();
-    request.open('GET', 'data.txt', false);
+    request.open('GET', 'data.txt');
 
     request.onreadyStateChange = function () {
-
-
         // document.getElementById('demo').innerHTML = request.responseText;
         if (request.status === 200 && request.readyState === 4) {
             console.log(request);
