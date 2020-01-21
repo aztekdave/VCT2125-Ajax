a367d32470b0a8bb06b758fc7435fa8d049d94ba
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-01-21 11:00:18 -0600

    added data to data.xml

---
+++ Unit2/script.js
@@ -0,0 +1,16 @@
+var request;
+if(window.XMLHttpRequest){
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject('Microsoft.XMLHTTP');
+}
+request.open('GET', 'data.xml');
+
+request.onreadystatechange = function () {
+    // document.getElementById('demo').innerHTML = request.responseText;
+    if (request.status === 200 && request.readyState ===4 ) {
+        console.log(request);
+        document.writeln(request.responseText);
+    }
+}
+request.send();
