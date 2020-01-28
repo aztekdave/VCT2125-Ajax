99ef3be99a64b9b4b8f348ff3a6cff4ce7c4630b
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-01-28 11:55:34 -0600

    added unit 3 - json

---
+++ Unit 3/script.js
@@ -0,0 +1,30 @@
+function loadAjax() {
+    var request;
+if(window.XMLHttpRequest){
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject('Microsoft.XMLHTTP');
+}
+request.open('GET', 'data.json');
+
+request.onreadystatechange = function () {
+
+    if ((request.status === 200 && request.readyState === 4)) {
+        
+        var items = JSON.parse(request.responseText);
+        console.log(items);
+        var output = '<ul>';
+        for(var key in items) {
+            output += '<li>' + items[key].artistname + '</li>';
+        }
+
+        // for (var i = 0; i < items.length; i++) {
+        //     output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+        // }
+        output += '</ul>';
+        document.getElementById('update').innerHTML = output;
+    }
+}
+request.send();
+
+}
