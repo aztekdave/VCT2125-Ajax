c5036bcfa468f16d77ce5808421a356d7bb156a6
Davey Schaaf <sf9822io@e2381-as1750361.century.edu>
2020-03-03 11:13:20 -0600

    updated react files

---
+++ unit8-react-demo/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+clas Message extends React.Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <h1 style={{color: this.props.color}}>
+                    {this.props.msg}
+                    </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Message color="blue" msg="how are you?"/>,
+    document.getElementById('root')
+)
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
